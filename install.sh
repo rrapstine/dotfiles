6a84892da538d9844e235baec7e6cfb4ad694a90
Richard Rapstine III <compostkicker@richards-mbp.lan>
2018-03-12 16:42:19 -0500

    Remove caffeinate option

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Keep Mac from sleeping until finished
-echo "Starting caffeine IV..."
-caffeinate
-
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
