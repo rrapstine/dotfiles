fb0f0a32abde7f66eb9c15273acbcc369bb52445
Richard Rapstine <richard.rapstine@gmail.com>
2025-09-08 01:39:22 -0500

    Revamped SSH setup. Added cloning wezterm config. Tweaked paths for dotfiles

--- install.sh
+++ install.sh
@@ -22,8 +22,7 @@ fi
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle --file $DOTFILES/Brewfile -v
+brew bundle --file ./Brewfile -v
 
 # Function to check SSH setup with GitHub
 check_ssh() {
@@ -32,32 +31,14 @@ check_ssh() {
         echo "SSH is already set up with GitHub."
     else
         echo "SSH is not set up with GitHub. Setting it up now..."
-        setup_ssh
+        ./ssh.sh
     fi
-}
-
-# Function to set up SSH keys
-setup_ssh() {
-    # Check if an ED25519 SSH key already exists
-    if [ ! -f "$HOME/.ssh/id_ed25519.pub" ]; then
-        echo "No ED25519 SSH key found. Generating a new SSH key..."
-        ssh-keygen -t ed25519 -C "your_email@example.com" -N "" -f "$HOME/.ssh/id_ed25519"
-    else
-        echo "ED25519 SSH key already exists. Skipping key generation."
-    fi
-
-    # Display the public key
-    echo "Copy the following SSH public key to your GitHub account:"
-    cat "$HOME/.ssh/id_ed25519.pub"
-
-    echo "Once you have added the key to https://github.com/settings/keys, press Enter to continue."
-    read -p ""
 }
 
 # Ensure SSH is set up before cloning Fish repository
 check_ssh
 
-# Ensure that my fish config is pulled down into the proper location
+# Clone my fish shell config down
 if [[ -d "$HOME/.config/fish" ]]; then
   echo "Fish is already configured"
 else
@@ -74,6 +55,13 @@ else
     git clone git@github.com:rrapstine/nvim.git $HOME/.config/nvim
 fi
 
+# Clone my WezTerm config down
+if [[ -d "$HOME/.config/wezterm" ]]; then
+    echo "Wezterm is already configured"
+else
+    git clone git@github.com:rrapstine/wezterm.git $HOME/.config/wezterm
+fi
+
 # Create a Code directory and subdirectories
 mkdir $HOME/Code
 mkdir $HOME/Code/projects
@@ -84,10 +72,10 @@ mkdir $HOME/Code/templates
 # $DOTFILES/clone.sh
 
 # Symlink the Mackup config file to the home directory
-ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+# ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
 
 # Set dock using dockutil
 sh dock.sh
 
 # Set macOS preferences - we will run this last because this will reload the shell
-source $DOTFILES/.macos
+source ./.macos
