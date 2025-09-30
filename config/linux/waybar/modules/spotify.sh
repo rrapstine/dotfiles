fce496f2574e7a2d90e84ef6ce2f53abcaee5a5e
Richard F Rapstine III <richard.rapstine@gmail.com>
2025-09-29 19:39:47 -0500

    enhance: improve media controls with Spotify priority and dedicated workspace
    - Media keys now prioritize Spotify while maintaining fallback to other players
    - Add SUPER+play/pause keybind to launch Spotify in dedicated Media workspace
    - Enable scrolling text in waybar for long track titles
    - Create workspace 10 as dedicated Media workspace with window rules for Spotify

--- config/linux/waybar/modules/spotify.sh
+++ config/linux/waybar/modules/spotify.sh
@@ -5,9 +5,6 @@ icon=""
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-  if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-40)"..."
-  fi
   text=$info" "$icon
 elif [[ $class == "paused" ]]; then
   text=$icon
