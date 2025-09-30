484dc88244a1ea946a103c7a4cc68b89245ab612
Richard F Rapstine III <richard.rapstine@gmail.com>
2025-09-29 20:15:06 -0500

    feat: enhance wezterm tab navigation and fix key conflicts
    - Add numpad support (Numpad1-9) for tab switching alongside number row
    - Replace bracket keys with arrow keys for tab navigation to avoid conflicts
    - Move copy mode activation from 'b' to 'v' key to prevent conflicts
    - Fix git submodule issue with wezterm config directory

---
+++ config/universal/wezterm/plugins/tabline.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm'
+local tabline = wezterm.plugin.require 'https://github.com/michaelbrusegard/tabline.wez'
+
+tabline.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'Catppuccin Mocha',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thick,
+      right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thick,
+      right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.ple_right_half_circle_thick,
+      right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    },
+  },
+  sections = {
+    tabline_a = { 'domain' },
+    tabline_b = { 'workspace' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = {},
+    tabline_y = {},
+    tabline_z = {},
+  },
+  extensions = {},
+}
