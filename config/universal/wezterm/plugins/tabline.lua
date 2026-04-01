23a261c7911e718f9ee041ffee3e8ca12af716b9
Richard F Rapstine III <richard.rapstine@gmail.com>
2026-04-01 10:02:27 -0500

    rebase wezterm config to original state, prior to revamp

--- config/universal/wezterm/plugins/tabline.lua
+++ config/universal/wezterm/plugins/tabline.lua
@@ -1,21 +1,7 @@
-local wezterm = require('wezterm')
-local tabline = wezterm.plugin.require('https://github.com/michaelbrusegard/tabline.wez')
+local wezterm = require 'wezterm'
+local tabline = wezterm.plugin.require 'https://github.com/michaelbrusegard/tabline.wez'
 
-local function workspace_process(tab)
-  local workspace = 'default'
-  
-  local mux_win = tab.window
-  if mux_win then
-    workspace = mux_win:get_workspace() or 'default'
-  end
-  
-  local process = tab.active_pane.foreground_process_name or 'shell'
-  process = process:match('([^/]+)$') or process
-  
-  return workspace .. ' | ' .. process
-end
-
-tabline.setup({
+tabline.setup {
   options = {
     icons_enabled = true,
     theme = 'Catppuccin Mocha',
@@ -35,21 +21,20 @@ tabline.setup({
     },
   },
   sections = {
-    tabline_a = { 'mode' },
+    tabline_a = { 'domain' },
     tabline_b = { 'workspace' },
     tabline_c = { ' ' },
     tab_active = {
       'index',
-      { workspace_process, padding = { left = 1, right = 1 } },
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
       { 'zoomed', padding = 0 },
     },
-    tab_inactive = {
-      'index',
-      { workspace_process, padding = { left = 1, right = 1 } },
-    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
     tabline_x = {},
     tabline_y = {},
     tabline_z = {},
   },
   extensions = {},
-})
+}
